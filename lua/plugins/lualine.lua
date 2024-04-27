02c36529f8ab54efe581f86d5d3d8ac3cda3411f
Peter Sanders <peter@psanders.me>
2024-04-27 19:44:59 -0400

    cfg: Enable icons for lualine
    The lualine documentation specifies that it requires a patched font for
    icon support. Icons were initially disabled because I was unsure what
    that meant, but "patched font" refers to a font patched by Nerd Fonts
    Patcher [1]. With a supported font installed, icons have been enabled.
    
    [1]: https://github.com/ryanoasis/nerd-fonts

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
 	require("lualine").setup({
-	    icons_enabled = false,
+	    icons_enabled = true,
 	})
     end,
 }
