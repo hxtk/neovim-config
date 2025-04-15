1763ddf84a30cbe34be0a58be8733203176076bd
Peter Sanders <peter@psanders.me>
2025-04-15 01:54:43 -0400

    feat: Trigger auto-completion on-demand

--- lua/mylsps.lua
+++ lua/mylsps.lua
@@ -3,8 +3,11 @@ return {
         vim.api.nvim_create_autocmd("LspAttach", {
             callback = function(ev)
                 local client = vim.lsp.get_client_by_id(ev.data.client_id)
-                if client:supports_method("textDocument/completion") then
-                    vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+                if client and client:supports_method("textDocument/completion") then
+                    vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
+                    vim.keymap.set("i", "<c-n>", function()
+                        vim.lsp.completion.get()
+                    end)
                 end
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
                 vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, {})
